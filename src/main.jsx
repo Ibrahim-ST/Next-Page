0af4d13979574dbdcd1b8ed6f0ef3f4de886c1fc
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2023-04-08 14:36:49 +0600

    finished navbar of nextpage project

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import About from "./components/About";
+import Books from "./components/Books";
+import Home from "./components/Home";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/books",
+        element: <Books />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}></RouterProvider>
+);
